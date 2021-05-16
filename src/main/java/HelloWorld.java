7c249fcae75880497f5e71699ba316ecdeae60af
Olga Bras <ola.spinaker@gmail.com>
2021-05-16 20:48:25 +0200

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+ public static void main(String[] args) {
+    System.out.println("Hello world");
+    System.out.println("My name is Olga");
+    System.out.println("My age is 18");
+    System.out.println("B");
+}
+
+}
+
+
