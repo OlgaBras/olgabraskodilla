d73ed400c7f9be9f4fb5a7d08eef6fe4d478d25d
Olga Bras <ola.spinaker@gmail.com>
2021-05-21 15:07:53 +0200

    DebugExample

--- kodilla-intro/src/main/java/Main.java
+++ kodilla-intro/src/main/java/Main.java
@@ -1,12 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-            String myName = "Maciej";
-            int myAge = 22;
-            double height = 1.78;
-            char carClass = 'A';
-            boolean isGoodProgrammer = true;
-        System.out.println(myName);
+ public static void main(String[] args) {
+    String myName = "Olga";
+    int myAge = 32;
+    double height = 1.75;
+    char carClass = 'A';
+    boolean isGoodProgrammer = true;
+    System.out.println("My name is " + myName);
+    System.out.println("My age is " + myAge);
+    System.out.println("My age is " + height);
+    System.out.println("My age is " + carClass);
+    System.out.println("My age is " + isGoodProgrammer);
 
-        }
-    }
+}
 }
