c0af0dfe5cb1c78a7b1996364b26ee1c0211a069
Olga Bras <ola.spinaker@gmail.com>
2021-06-08 14:17:50 +0200

    bb

---
+++ kodilla-intro/src/main/java/ApplicationBook.java
@@ -0,0 +1,6 @@
+public class ApplicationBook {
+    public static void main(String[] args) {
+        Book book = Book.of(author: "Jan Kowalski",title: "Java 7");
+
+    }
+}
---
+++ kodilla-intro/src/main/java/Book.java
@@ -0,0 +1,21 @@
+public class Book {
+private String author;
+private String title;
+
+public static Book of(String author, String title) {
+    return new Book(author,title);
+
+}
+private Book(String author, String title) {
+    this.author;
+    this.title;
+}
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
